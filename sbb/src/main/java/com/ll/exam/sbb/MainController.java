a3641260a619d355ce42439ea62fba005b0c00eb
jeonga-Son <jjeong0106@gmail.com>
2022-08-08 15:53:08 +0900

    ResponseBody

--- sbb/src/main/java/com/ll/exam/sbb/MainController.java
+++ sbb/src/main/java/com/ll/exam/sbb/MainController.java
@@ -3,12 +3,17 @@ package com.ll.exam.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    //get, post 매핑 모두 포함하는 것 => @RequestMapping
-    @RequestMapping("/sbb")
-    public void index() {
-        System.out.println("index");
-    }
+
+    @RequestMapping("/sbb")  //get, post 매핑 모두 포함하는 것 => @RequestMapping
+    @ResponseBody
+    // 아래 함수의 리턴값을 그대로 브라우저에 표시
+    // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답의 바디에 담는다.
+    public String index() {
+        System.out.println("Hello");  // 콘솔에 출력됨. 서버에서 실행.
+        return "안녕하세요."; // 브라우저에 출력됨. 먼 미래에 브라우저에서 보여짐.
+     }
 }
