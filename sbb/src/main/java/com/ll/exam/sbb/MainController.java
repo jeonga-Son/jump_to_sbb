93836e351f2ed4d2400a7b53d49e897dc48321d6
jeonga-Son <jjeong0106@gmail.com>
2022-08-09 14:47:43 +0900

    action에서 자동으로 조립된 객체를 입력 받기

--- sbb/src/main/java/com/ll/exam/sbb/MainController.java
+++ sbb/src/main/java/com/ll/exam/sbb/MainController.java
@@ -215,6 +215,20 @@ public class MainController {
 
         return "%d번 게시물을 삭제하였습니다.".formatted(article.getId());
     }
+
+    @GetMapping("addPersonOldWay")
+    @ResponseBody
+    Person addPersonOldWay(int id, int age, String name) {
+        Person p = new Person(id, age, name);
+
+        return p;
+    }
+
+    @GetMapping("addPerson")
+    @ResponseBody
+    Person addPerson(Person p) {
+        return p;
+    }
 }
 
     @AllArgsConstructor
@@ -229,7 +243,13 @@ public class MainController {
         public Article(String title, String body) {
             this(++lastId, title, body);
         }
-
 }
 
+@AllArgsConstructor
+@Getter
+class Person {
+    private int id;
+    private int age;
+    private String name;
+};
 
