4ff4c0563a7caa191c25c3540d5ef24ee08ed2b5
jeonga-Son <jjeong0106@gmail.com>
2022-08-10 11:46:03 +0900

    Answer 클래스 추가

---
+++ sbb/src/main/java/com/ll/exam/sbb/Answer.java
@@ -0,0 +1,24 @@
+package com.ll.exam.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne //이거를 붙여야 됨.
+    private Question question; //question에 참조값이 들어감
+}
