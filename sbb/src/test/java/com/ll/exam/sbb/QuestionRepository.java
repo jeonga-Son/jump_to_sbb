a6d1638d5d0dbe05b4250afcfb26c9ebb45f3f12
jeonga-Son <jjeong0106@gmail.com>
2022-08-10 14:48:32 +0900

    findBySubjectAndContent

--- sbb/src/test/java/com/ll/exam/sbb/QuestionRepository.java
+++ sbb/src/test/java/com/ll/exam/sbb/QuestionRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     Question findBySubject(String s);
+
+    Question findBySubjectAndContent(String subject, String content);
 }
