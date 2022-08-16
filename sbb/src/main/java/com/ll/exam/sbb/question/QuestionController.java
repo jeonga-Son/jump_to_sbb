c1e970210f06e2a5718284b4b123a15a929ee8fc
jeonga-Son <jjeong0106@gmail.com>
2022-08-16 15:41:54 +0900

    질문 리스트 템플릿 작업

--- sbb/src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,28 +1,26 @@
 package com.ll.exam.sbb.question;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import java.util.List;
 @Controller
 @RequiredArgsConstructor // 생성자 주입
 public class QuestionController {
-    //@Autowired // 필드주입
-    //여기에 final 붙이고 @RequiredArgsConstructor 붙여주면 final 붙은것들은 자동으로 @Autowired가 됨.
+    // @Autowired // 필드 주입
     private final QuestionRepository questionRepository;
-
-
     @RequestMapping("/question/list")
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
-    public String list() {
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        // 미래에 실행된 question_list.html 에서
+        // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
+        model.addAttribute("questionList", questionList);
 
         return "question_list";
     }
 
-    @RequestMapping("/question/list2")
-    @ResponseBody
-    public String list2() {
-        return "리스트2 입니다!!!";
-    }
 }
