8caac4ca34bdc24e877efadde9b8f336f064bef3
jeonga <jjeong0106@gmail.com>
2022-08-17 14:52:39 +0900

    답변 폼 체크

--- sbb/src/main/java/com/ll/exam/sbb/question/QuestionForm.java
+++ sbb/src/main/java/com/ll/exam/sbb/question/QuestionForm.java
@@ -2,6 +2,7 @@ package com.ll.exam.sbb.question;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
