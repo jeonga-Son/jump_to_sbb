cca63f85887acb0997749999e04257e96a13573a
jeonga <jjeong0106@gmail.com>
2022-08-17 13:40:51 +0900

    질문 등록 처리

--- sbb/src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -4,6 +4,7 @@ import com.ll.exam.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,4 +22,12 @@ public class QuestionService {
                 .orElseThrow(() -> new DataNotFoundException("no %d question not found,".formatted(id)));
     }
 
+    public void create(String subject, String content) {
+        Question q = new Question();
+
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q);
+    }
 }
