13d0736f21dbb0ee7f4ed39d16b5ea8cba29604c
jeonga <jjeong0106@gmail.com>
2022-08-17 09:44:39 +0900

    QuestionController 에 URL 프리픽스, 답변 등록 폼 구현

--- sbb/src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
+
+@RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor // 생성자 주입
 // 컨트롤러는 Repository가 있는지 몰라야 한다.
@@ -19,7 +21,7 @@ import java.util.List;
 public class QuestionController {
     // @Autowired // 필드 주입
     private final QuestionService questionService;
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
     public String list(Model model) {
         List<Question> questionList = questionService.getList();
@@ -30,7 +32,7 @@ public class QuestionController {
         return "question_list";
     }
 
-    @RequestMapping("/question/detail/{id}")
+    @RequestMapping("/detail/{id}")
     //model 객체는 Controller에서 생성한 데이터를 담아서 View로 전달할 때 사용하는 객체이다.
     //addAttribute("키", "값") 메소드를 사용하여 전달할 데이터를 세팅한다.
     public String detail(Model model, @PathVariable int id) {
--- sbb/src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -16,10 +16,10 @@ public class QuestionService {
         return this.questionRepository.findAll();
     }
 
-    public Question getQuestion(int id) {
+    public Question getQuestion(int id) throws DataNotFoundException {
         Optional<Question> oq = questionRepository.findById(id);
 
-        if ( oq.isPresent() ) {
+        if (oq.isPresent()) {
             return oq.get();
         }
 
