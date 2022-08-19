138dbe054e567930567dbfeed9653240bdec2d7d
jeonga <jjeong0106@gmail.com>
2022-08-19 14:30:55 +0900

    회원관련 테스트케이스를 실행하기 전, 답변, 질문, 회원 순으로 삭제, 회원 2명 생성 후 진행

--- sbb/src/test/java/com/ll/exam/sbb/UserServiceTests.java
+++ sbb/src/test/java/com/ll/exam/sbb/UserServiceTests.java
@@ -1,6 +1,10 @@
 package com.ll.exam.sbb;
 
+import com.ll.exam.sbb.answer.AnswerRepository;
+import com.ll.exam.sbb.question.QuestionRepository;
+import com.ll.exam.sbb.user.UserRepository;
 import com.ll.exam.sbb.user.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +12,51 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class UserServiceTests {
-
     @Autowired
     private UserService userService;
 
-    @Test@DisplayName("회원가입이 가능하다.")
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        clearData();
+        createSampleData();
+    }
+
+    public static void createSampleData(UserService userService) {
+        userService.create("admin", "admin@test.com", "1234");
+        userService.create("user1", "user1@test.com", "1234");
+    }
+
+    private void createSampleData() {
+        createSampleData(userService);
+    }
+
+    public static void clearData(UserRepository userRepository, AnswerRepository answerRepository, QuestionRepository questionRepository) {
+        AnswerRepositoryTests.clearData(answerRepository, questionRepository);
+        QuestionRepositoryTests.clearData(questionRepository);
+        userRepository.deleteAll(); // DELETE FROM site_user;
+        userRepository.truncateTable();
+    }
+
+    private void clearData() {
+        clearData(userRepository, answerRepository, questionRepository);
+    }
+
+    @Test
+    @DisplayName("회원가입이 가능하다.")
     public void t1() {
-        userService.create("user1", "user@email.com", "1234");
+        userService.create("user2", "user2@email.com", "1234");
     }
 }
+
+
+//SiteUser가 있어야 => Question 있을수있음 => Answer 있을 수 있음.
+// 지우는건 반대로!
