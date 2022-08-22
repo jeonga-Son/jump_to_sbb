290b4b02d5cf54575523c15d719385f333aced3a
jeonga <jjeong0106@gmail.com>
2022-08-22 15:34:54 +0900

    질문 수정 화면

--- sbb/src/main/java/com/ll/exam/sbb/question/QuestionForm.java
+++ sbb/src/main/java/com/ll/exam/sbb/question/QuestionForm.java
@@ -9,7 +9,8 @@ import javax.validation.constraints.Size;
 
 
 //@AllArgsConstructor 어노테이션은 모든 필드 값을 파라미터로 받는 생성자를 만들어줍니다.
-@AllArgsConstructor
+//@AllArgsConstructor
+@Setter
 @Getter
 public class QuestionForm {
     @NotEmpty(message = "제목은 필수항목입니다.")
@@ -17,4 +18,5 @@ public class QuestionForm {
     private String subject;
     @NotEmpty(message = "내용은 필수항목입니다.")
     private String content;
+
 }
