13ae5df6350e2db66225dfe07450dd0dac1ee9c6
jeonga <jjeong0106@gmail.com>
2022-08-23 15:23:07 +0900

    답변 수정, 추천 후에도 해당 답변으로 스크롤 되도록

--- sbb/src/main/java/com/ll/exam/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -73,7 +73,7 @@ public class AnswerController {
 
         answerService.modify(answer, answerForm.getContent());
 
-        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
+        return "redirect:/question/detail/%d#answer_%d".formatted(answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -97,6 +97,6 @@ public class AnswerController {
         SiteUser siteUser = userService.getUser(principal.getName());
 
         answerService.vote(answer, siteUser);
-        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
+        return "redirect:/question/detail/%d#answer_%d".formatted(answer.getQuestion().getId(), answer.getId());
     }
 }
