5b94e6baf70907a688a04fe0cd1908f1612b1be5
jeonga <jjeong0106@gmail.com>
2022-08-25 11:22:47 +0900

    kw 파라미터가 질문제목 검색어로 작동

--- sbb/src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
@@ -1,6 +1,8 @@
 package com.ll.exam.sbb.question;
 
 import com.ll.exam.sbb.base.RepositoryUtil;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -21,4 +23,5 @@ public interface QuestionRepository extends JpaRepository<Question, Long>, Repos
     @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true) //이후에 데이터를 추가했을 때 1번부터 나오도록.
     void truncate(); // 이거 지우면 안됨, truncateTable 하면 자동으로 이게 실행됨
 
+    Page<Question> findBySubjectContains(String kw, Pageable pageable);
 }
