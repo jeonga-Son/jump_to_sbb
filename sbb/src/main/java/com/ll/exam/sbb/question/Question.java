3aaf64538b55dd3bbef5e974f5e31da7d6959535
jeonga <jjeong0106@gmail.com>
2022-08-29 09:18:04 +0900

    조회수 칼럼 추가

--- sbb/src/main/java/com/ll/exam/sbb/question/Question.java
+++ sbb/src/main/java/com/ll/exam/sbb/question/Question.java
@@ -42,6 +42,8 @@ public class Question {
     @ManyToMany
     Set<SiteUser> voter;
 
+    private Integer hitCount = 0;
+
     public void addAnswer(Answer answer) {
         answer.setQuestion(this);
         getAnswerList().add(answer);
